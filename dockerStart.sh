ec7e7d2614c530bea2692cfbf62035cf7cd0db5d
monksy <crashoverride025@gmail.com>
2020-04-26 23:58:10 -0500

    Update dockerStart.sh
    Co-Authored-By: Chris Egerton <fearthecellos@gmail.com>

--- dockerStart.sh
+++ dockerStart.sh
@@ -2,7 +2,7 @@
 
 # We're going to override the subreddit, comments, and broker if we have an environment variable set
 
-[[ -v SUBREDDIT ]] && echo "posts.subreddits=$SUBREDDIT" >> /kafka-connect-reddit/kafka-connect-reddit-source.properties
+[[ -v SUBREDDIT ]] && sed -ir 's/^posts\.subreddits=.*$/posts.subreddits='"$SUBREDDIT/" kafka-connect-reddit.properties
 [[ -v COMMENTS_SUBREDDIT ]] && echo "comments.subreddits=$COMMENTS_SUBREDDIT" >> /kafka-connect-reddit/kafka-connect-reddit-source.properties
 [[ -v KAFKA_SERVER ]] && echo "bootstrap.servers=$KAFKA_SERVER" >> /kafka-connect-reddit/connect-standalone.properties
 
